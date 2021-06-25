831cca23d76a0a2924d2b6054b52c9188e244ce5
Alina Bovdyr <alinabovdyr92@gmail.com>
2021-06-26 00:55:51 +0300

    Updates

--- static/js/favorites-page.6d49bcce.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonphw-task-foodify-application"]=this["webpackJsonphw-task-foodify-application"]||[]).push([[0],{72:function(i,o){}}]);
-//# sourceMappingURL=favorites-page.6d49bcce.chunk.js.map
